acc91302837dc5c3b501ab5da909842394740477
Santyzz0311 <santyg0311@gmail.com>
2024-11-24 23:22:01 -0500

    fix: nativewind config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 // here your entry point layout
 
+import '../global.css'
 import { Slot } from 'expo-router'
 
 export default function Layout() {
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,13 @@
 // here your app entry point
 
+import { Text, View } from 'react-native'
+
 export default function Index() {
-  return <></>
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-200">
+      <Text className="text-xl font-bold text-black/80">
+        Here you can start coding!
+      </Text>
+    </View>
+  )
 }
